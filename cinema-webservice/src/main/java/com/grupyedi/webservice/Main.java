7c7f9774901b0e28b4e9f631218284952d27db9b
xinoip <pionix@protonmail.com>
2020-06-07 20:39:49 +0300

    Data Access Object (DAO) Pattern for dummy entity: - Dao classes abstracts the hibernate process

--- cinema-webservice/src/main/java/com/grupyedi/webservice/Main.java
+++ cinema-webservice/src/main/java/com/grupyedi/webservice/Main.java
@@ -2,41 +2,15 @@ package com.grupyedi.webservice;
 
 import java.util.List;
 
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
 import com.grupyedi.webservice.entity.Student;
-import com.grupyedi.webservice.util.HibernateUtil;
+import com.grupyedi.webservice.dao.StudentDao;
 
 public class Main {
     public static void main(String[] args) {
-
+        StudentDao studentDao = new StudentDao();
         Student student = new Student("Ramesh", "Fadatare", "rameshfadatare@javaguides.com");
-        Student student1 = new Student("John", "Cena", "john@javaguides.com");
-        Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            // start a transaction
-            transaction = session.beginTransaction();
-            // save the student objects
-            session.save(student);
-            session.save(student1);
-            // commit transaction
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
-
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            List<Student> students = session.createQuery("from Student", Student.class).list();
-            students.forEach(s -> System.out.println(s.getFirstName()));
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
+        studentDao.saveStudent(student);
+        List < Student > students = studentDao.getStudents();
+        students.forEach(s -> System.out.println(s.getFirstName()));
     }
 }
