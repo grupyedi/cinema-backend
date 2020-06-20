d25776534ad14a42ca68beef82591d321ef81a91
xinoip <pionix@protonmail.com>
2020-06-21 02:54:31 +0300

    changed main launch procedure: - first launch the DB processes - then try to launch the server - if configs fail, the server may not launch successfuly

--- src/main/java/com/grupyedi/webservice/Main.java
+++ src/main/java/com/grupyedi/webservice/Main.java
@@ -1,10 +1,12 @@
 package com.grupyedi.webservice;
 
+import java.io.IOException;
 import java.sql.Date;
 import java.util.List;
 
 import com.grupyedi.webservice.dao.DaoManager;
 import com.grupyedi.webservice.entity.*;
+import com.grupyedi.webservice.server.ServerLauncher;
 import com.grupyedi.webservice.util.HibernateUtil;
 import com.grupyedi.webservice.util.SeedUtil;
 
@@ -14,6 +16,12 @@ public class Main {
 
         SeedUtil.seedDb();
 
-        HibernateUtil.shutdown();
+        try {
+            ServerLauncher serverLauncher = new ServerLauncher();
+            System.out.println("Server launched");
+        } catch (IOException e) {
+            System.out.println("Server failed to launch!");
+            e.printStackTrace();
+        }
     }
 }
