ca7c70647a969e6595d64b939a4c1b7edd481d13
xinoip <pionix@protonmail.com>
2020-06-20 23:58:42 +0300

    Movie entity/table

--- src/main/java/com/grupyedi/webservice/entity/Movie.java
+++ src/main/java/com/grupyedi/webservice/entity/Movie.java
@@ -2,39 +2,101 @@ package com.grupyedi.webservice.entity;
 
 import javax.persistence.*;
 import java.sql.Date;
-import java.util.List;
 
 @Entity
-@Table(name = "t_movie")
+@Table(name = "movie")
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public int id;
+    @Column(name = "id")
+    private int id;
 
-    @Column(name = "c_name")
-    public String name;
+    @Column(name = "name")
+    private String name;
 
-    @Column(name = "c_banner_urls")
-    @ElementCollection(targetClass = String.class)
-    public List<String> bannersUrls;
+    // TODO: add banner urls as list
 
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "c_genre_id")
-    public Genre genre;
+    @JoinColumn(name = "genre_id")
+    private Genre genre;
 
-    @Column(name = "c_summary")
-    public String summary;
+    @Column(name = "summary")
+    private String summary;
 
-    @Column(name = "c_director")
-    public String director;
+    @Column(name = "director")
+    private String director;
 
-    @Column(name = "c_actors")
-    @ElementCollection(targetClass = String.class)
-    public List<String> actors;
+    // TODO: add actors as list
 
-    @Column(name = "c_age_rating")
-    public int ageRating;
+    @Column(name = "ageRating")
+    private int ageRating;
 
-    @Column(name = "c_release_date")
-    public Date releaseDate;
+    @Column(name = "releaseDate")
+    private Date releaseDate;
+
+    public Movie() {}
+
+    public Movie(String name, String summary, String director, int ageRating, Date releaseDate) {
+        this.name = name;
+        this.summary = summary;
+        this.director = director;
+        this.ageRating = ageRating;
+        this.releaseDate = releaseDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getAgeRating() {
+        return ageRating;
+    }
+
+    public void setAgeRating(int ageRating) {
+        this.ageRating = ageRating;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
 }
