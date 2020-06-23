0367678fa7fa819339df401b411d45e9e8c4391a
xinoip <pionix@protonmail.com>
2020-06-23 23:38:20 +0300

    Fixed purchase endpoint json parsing issue

--- src/main/java/com/grupyedi/webservice/Main.java
+++ src/main/java/com/grupyedi/webservice/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
         HibernateUtil.getSessionFactory();
 
-        SeedUtil.seedDb();
+//        SeedUtil.seedDb();
 
         try {
             ServerLauncher serverLauncher = new ServerLauncher();
